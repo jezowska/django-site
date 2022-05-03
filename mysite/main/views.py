40e101b9981dd92a20da41888afc3db9a6edde3d
Daria <daria.jezowska@gmail.com>
2022-05-03 14:39:52 +0200

    Add forms

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
+from .forms import CreateNewList
 
 # Create your views here.
 
@@ -9,3 +10,20 @@ def index(response, id):
     return render(response, "main/list.html", {"ls":ls})
 def home(response):
     return render(response, "main/home.html", {})
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name = n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+
+    else:
+        form = CreateNewList()
+    return render(response, "main/create.html", {"form":form})
+
+
